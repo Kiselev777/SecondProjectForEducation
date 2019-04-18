c173a913cbe02da89d370c4d781a39f8f2faabb6
Kiselev777 <forzorezor1@mail.ru>
2019-04-18 20:08:09 +0300

    Correct

--- src/test/java/FileDivTest.java
+++ src/test/java/FileDivTest.java
@@ -12,83 +12,83 @@ public class FileDivTest {
         return super.equals(obj);
     }
 
-    ClassLoader classLoader = getClass().getClassLoader();
 
-    static boolean fileEquals(File file, File file2) throws IOException {
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader(file));
-            BufferedReader reader2 = new BufferedReader(new FileReader(file2));
-            String s = reader.readLine();
-            String s2 = reader2.readLine();
-            while (true) {
-                if ((s == null && s2 != null) || (s != null && s2 == null))
-                    return false;
-                if (s == null && s2 == null)
-                    break;
-                if (!s.equals(s2))
-                    return false;
-                s = reader.readLine();
-                s2 = reader2.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            throw e;
+    static boolean fileEquals(File file, File file2) throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        BufferedReader reader2 = new BufferedReader(new FileReader(file2));
+        String s = reader.readLine();
+        String s2 = reader2.readLine();
+        while (true) {
+            if ((s == null && s2 != null) || (s != null && s2 == null))
+                return false;
+            if (s == null && s2 == null)
+                break;
+            if (!s.equals(s2))
+                return false;
+            s = reader.readLine();
+            s2 = reader2.readLine();
         }
         return true;
     }
 
+    File createThisFilePLZ(String s) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        return new File(Objects.requireNonNull(classLoader.getResource(s)).getFile());
+    }
+
     @Test(expected = IllegalArgumentException.class)
-    public void lineZero() throws IOException {
+    public void lineZero() throws Exception {
         new FileDiv("obama.txt", "kisilevPresident", 0, true, -1, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliks1.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), createThisFilePLZ("kiseliks1.txt")));
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void fileZero() throws IOException {
+    public void fileZero() throws Exception {
         new FileDiv("obama.txt", "kisilevPresident", -1, false, 0, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliksa.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), createThisFilePLZ("kiseliksa.txt")));
 
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void charZero() throws IOException {
+    public void charZero() throws Exception {
         new FileDiv("obama.txt", "kisilevPresident", -1, false, -1, 0).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliks123a.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), createThisFilePLZ("kiseliks123a.txt")));
 
 
     }
 
-    @Test(expected = IllegalArgumentException.class )
-    public void inputNameError() throws IOException {
+    @Test(expected = IllegalArgumentException.class)
+    public void inputNameError() throws Exception {
         new FileDiv("someFiles", "kisilevPresident", -1, false, -1, 0).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliks123a.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), createThisFilePLZ("kiseliks123a.txt")));
 
 
     }
 
 
     @Test
-    public void run() throws IOException {
+    public void run() throws Exception {
         new FileDiv("obama.txt", "kisilevPresident", 100, true, -1, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), new File(Objects.requireNonNull(classLoader.getResource("kisel1.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident2.txt"), new File(Objects.requireNonNull(classLoader.getResource("kisel2.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident3.txt"), new File(Objects.requireNonNull(classLoader.getResource("kisel3.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident4.txt"), new File(Objects.requireNonNull(classLoader.getResource("kisel4.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), createThisFilePLZ("kisel1.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident2.txt"), createThisFilePLZ("kisel2.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident3.txt"), createThisFilePLZ("kisel3.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident4.txt"), createThisFilePLZ("kisel4.txt")));
 
 
         new FileDiv("obama.txt", "kisilevPresident", -1, true, -1, 10000).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseldos1.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident2.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseldos2.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident1.txt"), createThisFilePLZ("kiseldos1.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident2.txt"), createThisFilePLZ("kiseldos2.txt")));
 
         new FileDiv("obama.txt", "kisilevPresident", -1, false, 2, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliks1234a.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidentb.txt"), new File(Objects.requireNonNull(classLoader.getResource("kiseliks1234b.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), createThisFilePLZ("kiseliks1234a.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidentb.txt"),  createThisFilePLZ("kiseliks1234b.txt")));
 
         new FileDiv("obama.txt", null, -1, false, 2, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), new File(Objects.requireNonNull(classLoader.getResource("xa.txt")).getFile())));
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidentb.txt"), new File(Objects.requireNonNull(classLoader.getResource("xb.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidenta.txt"), createThisFilePLZ("xa.txt")));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresidentb.txt"), createThisFilePLZ("xb.txt")));
 
         new FileDiv("obama.txt", "", 100, true, -1, -1).run();
-        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident4.txt"), new File(Objects.requireNonNull(classLoader.getResource("4.txt")).getFile())));
+        Assert.assertTrue(FileDivTest.fileEquals(new File("kisilevPresident4.txt"), createThisFilePLZ("4.txt")));
 
     }
 }
